89b96f4252564ced834eba9d88a80c1b87ec85b5
imen maazoun <imen.maazoun@gmail.com>
2024-06-28 10:30:17 +0200

    add file

--- src/test/java/org/m2i/contatct_backend_app/ContatctBackendAppApplicationTests.java
+++ src/test/java/org/m2i/contatct_backend_app/ContatctBackendAppApplicationTests.java
@@ -2,7 +2,6 @@ package org.m2i.contatct_backend_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 
 @SpringBootTest
 class ContatctBackendAppApplicationTests {
